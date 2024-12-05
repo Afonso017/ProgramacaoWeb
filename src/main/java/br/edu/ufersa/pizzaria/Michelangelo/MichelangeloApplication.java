91061aaa3400f7d27facba0486e05f9a87161335
Afonso017 <afonsosd016@gmail.com>
2024-12-05 16:06:28 -0300

    Commit inicial

---
+++ src/main/java/br/edu/ufersa/pizzaria/Michelangelo/MichelangeloApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.pizzaria.Michelangelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichelangeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MichelangeloApplication.class, args);
+	}
+
+}
