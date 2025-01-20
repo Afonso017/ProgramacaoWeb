e0dcdeebfdedcb14f185b50369e9277dab8dd5cf
Afonso017 <afonsosd016@gmail.com>
2025-01-20 20:04:33 -0300

    Página de login

--- src/main/java/br/edu/ufersa/pizzaria/FrontEnd/michelangelo_pizzaria/app/UI/TableHeader.js
+++
@@ -1,7 +0,0 @@
-const TableHeader = () => {
-    return (
-      <thead><tr><th>Dia da Semana</th><th>Disciplina</th><th>Horários</th></tr></thead>
-    );
-}
-
-export default TableHeader;
--- src/main/java/br/edu/ufersa/pizzaria/FrontEnd/michelangelo_pizzaria/app/page.js
+++ src/main/java/br/edu/ufersa/pizzaria/FrontEnd/michelangelo_pizzaria/app/page.js
@@ -1,37 +1,9 @@
-import Table from './UI/Table.js';
+import Login from './UI/Login/Login.js';
 
 export default function Home() {
-  const agendaSemanal = [
-    {
-      dia: 'Segunda-Feira',
-      disciplina: 'Programação Web',
-      horario: '08:00 às 11:30'
-    },
-    {
-      dia: 'Terça-Feira',
-      disciplina: 'Banco de Dados',
-      horario: '08:00 às 11:30'
-    },
-    {
-      dia: 'Quarta-Feira',
-      disciplina: 'Programação Web',
-      horario: '08:00 às 11:30'
-    },
-    {
-      dia: 'Quinta-Feira',
-      disciplina: 'Banco de Dados',
-      horario: '08:00 às 11:30'
-    },
-    {
-      dia: 'Sexta-Feira',
-      disciplina: 'Programação Web',
-      horario: '08:00 às 11:30'
-    }
-  ]
   return (
     <div>
-      <h1 className="Cleiton">Horários</h1>
-      <Table dados={agendaSemanal}/>
+        <Login />
     </div>
   );
 }
